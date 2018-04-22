9907bdf665824bf0e19d403097e99310108b0868
lfouts <lorenfouts@yahoo.com>
2018-04-21 22:48:19 -0400

    working code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,53 @@
-var topics = ["fail", "yas", "corgi", "side eye", "smh" ];
+$(document).ready(function() {
+
+  $("button").on("click", function() {
+    // In this case, the "this" keyword refers to the button that was clicked
+    var topics = $(this).attr("topic");
+
+    console.log(topics);
+
+    // Constructing a URL to search Giphy for the name of the person who said the quote
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+      topics + "&api_key=UqKLNuUwCYlpk5JIP7BxKsOn8XXOCFhV&limit=10";
+
+
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          })
+            // After the data comes back from the API
+            .then(function(response) {
+              // Storing an array of results in the results variable
+              var results = response.data;
+
+              // Looping over every result item
+              for (var i = 0; i < results.length; i++) {
+
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                  // Creating a div with the class "item"
+                  var gifDiv = $("<div class='item'>");
+
+                  // Storing the result item's rating
+                  var rating = results[i].rating;
+
+                  // Creating a paragraph tag with the result item's rating
+                  var p = $("<p>").text("Rating: " + rating);
+
+                  // Creating an image tag
+                  var gifImage = $("<img>");
+
+
+                  gifImage.attr("src", results[i].images.fixed_height.url);
+
+                  // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                  gifDiv.append(p);
+                  gifDiv.append(gifImage);
+
+                  // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                  $("#gifs-appear-here").prepend(gifDiv);
+                }
+              }
+            });
+            });
+        });
